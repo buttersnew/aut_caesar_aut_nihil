2e901d559e744f35f02b44642f7fdffd420ef74e
buttersnew <108363699+buttersnew@users.noreply.github.com>
2026-01-17 15:53:49 +0100

    Make some text a bit shorter

--- module_system/module_dialogs.py
+++ module_system/module_dialogs.py
@@ -7401,12 +7401,12 @@ dialogs =[
   (gt, ":spouse", 0),
   (str_store_troop_name, s12, ":spouse"),
 ],
-  "Have you lost your mind? I am already married with {s12}! And what if this causes the opposite reaction, what if he gets mad and tries to harm us! She stole that dagger from her father and gave it to you in exchange for this?",
+  "Have you lost your mind? I am already married with {s12}! What if he gets mad and tries to harm us! She stole that dagger from her father and gave it to you in exchange for this?",
 "wlodowiecus_3_sciri_2_4",[]],
 
 [anyone|plyr,"wlodowiecus_3_sciri_2_3",[
 ],
-  "Have you lost your mind?! What if this causes the opposite reaction, what if he gets mad and tries to harm us! She stole that dagger from her father and gave it to you in exchange for this?",
+  "Have you lost your mind?! What if this causes the opposite reaction? She stole that dagger from her father and gave it to you in exchange for this?",
 "wlodowiecus_3_sciri_2_4",[]],
 
 [anyone,"wlodowiecus_3_sciri_2_4",[
